04269d9268e01f72ae4a8813a262976e502d8be9
Manjinder Sandhu <sandhums@gmail.com>
2023-09-09 16:49:53 +0530

    Initial Commit

---
+++ TestIos/TestIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIosApp.swift
+//  TestIos
+//
+//  Created by Manjinder Sandhu on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TestIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
